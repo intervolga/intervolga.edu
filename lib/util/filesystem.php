2e6696df41ec9763dec30371d8d5df9a366e78bd
erofeev@intervolga.ru <erofeev@intervolga.ru>
2022-11-24 22:21:42 +0300

    Добавлен класс для работы с файловой системой сайта

---
+++ lib/util/filesystem.php
@@ -0,0 +1,93 @@
+<?php
+namespace Intervolga\Edu\Util;
+
+use Bitrix\Main\Application;
+use Bitrix\Main\IO\Directory;
+use Bitrix\Main\IO\File;
+use Bitrix\Main\IO\FileSystemEntry;
+
+class FileSystem
+{
+	const NON_PUBLIC_DIRS = [
+		'/upload/',
+		'/bitrix/',
+		'/local/',
+	];
+
+	/**
+	 * @param FileSystemEntry $entry
+	 * @return string
+	 */
+	public static function getLocalPath(FileSystemEntry $entry)
+	{
+		return str_replace(Application::getDocumentRoot(), '', $entry->getPath());
+	}
+
+	/**
+	 * @return Directory[]
+	 */
+	public static function getPublicDirsLevelOne()
+	{
+		$result = [];
+		$directory = new Directory(Application::getDocumentRoot());
+		foreach ($directory->getChildren() as $child) {
+			if ($child->isDirectory()) {
+				if (!in_array('/' . $child->getName() . '/', static::NON_PUBLIC_DIRS)) {
+					$result[] = $child;
+				}
+			}
+		}
+
+		return $result;
+	}
+
+	/**
+	 * @param Directory[] $dirs
+	 * @param string $namePart
+	 * @return File[]
+	 */
+	public static function getFilesRecursiveByNameSubst($dirs, $namePart)
+	{
+		return static::getFilesByNameSubst($dirs, $namePart, true);
+	}
+
+	/**
+	 * @param Directory[] $dirs
+	 * @param string $namePart
+	 * @return File[]
+	 */
+	public static function getFilesNonRecursiveByNameSubst($dirs, $namePart)
+	{
+		return static::getFilesByNameSubst($dirs, $namePart, false);
+	}
+
+	/**
+	 * @param Directory[] $dirs
+	 * @param string $namePart
+	 * @param bool $isRecursive
+	 * @return File[]
+	 */
+	protected static function getFilesByNameSubst($dirs, $namePart, $isRecursive)
+	{
+		$dirsToCheck = [];
+		$result = [];
+		foreach ($dirs as $dir) {
+			foreach ($dir->getChildren() as $child) {
+				if ($child->isDirectory()) {
+					if ($isRecursive) {
+						$dirsToCheck[] = $child;
+					}
+				} else {
+					if (substr_count($child->getName(), $namePart)) {
+						$result[] = $child;
+					}
+				}
+			}
+		}
+		if ($dirsToCheck) {
+			$result = array_merge($result, static::getFilesByNameSubst($dirsToCheck, $namePart, $isRecursive));
+		}
+
+		return $result;
+	}
+}
