e45ebd08fc8810711defec6750c0cbeec7193b18
erofeev <erofeev@intervolga.ru>
2022-11-24 18:07:34 +0300

    Исправлен код проверки Урока 1.1

--- lang/ru/lib/tests/courseone/lessonone/testupdates.php
+++ lang/ru/lib/tests/courseone/lessonone/testupdates.php
@@ -1,2 +1,2 @@
 <?php
-$MESS['INTERVOLGA_EDU.UPDATES_AVAILABLE'] = 'Доступно обновлений модулей: #COUNT#';
+$MESS['INTERVOLGA_EDU.UPDATES_AVAILABLE'] = 'Доступно обновлений модулей: #COUNT#. Последнее обновление: #LAST_UPDATE#';
