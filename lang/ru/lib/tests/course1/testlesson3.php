4868197547bc4482b2d68c806dc02f4f53c7ca51
erofeev@intervolga.ru <erofeev@intervolga.ru>
2022-11-24 23:03:50 +0300

    Добавлена проверка лишних шаблонов сайта

--- lang/ru/lib/tests/course1/testlesson3.php
+++ lang/ru/lib/tests/course1/testlesson3.php
@@ -1,2 +1,3 @@
 <?php
 $MESS['INTERVOLGA_EDU.TEST_COURSE_1_LESSON_3'] = 'Курс 1. Урок 3. Интеграция HTML шаблона';
+$MESS['INTERVOLGA_EDU.UNKNOWN_TEMPLATE_FOUND'] = 'Шаблон сайта <a href="#ADMIN_LINK#" target="_blank">#NAME#</a> нужно удалить';
--- lib/tests/course1/testlesson3.php
+++ lib/tests/course1/testlesson3.php
@@ -1,8 +1,12 @@
 <?php
 namespace Intervolga\Edu\Tests\Course1;
 
+use Bitrix\Main\Application;
+use Bitrix\Main\IO\Directory;
 use Bitrix\Main\Localization\Loc;
 use Intervolga\Edu\Tests\BaseTest;
+use Intervolga\Edu\Util\Admin;
+use Intervolga\Edu\Util\FileSystem;
 
 Loc::loadMessages(__FILE__);
 
@@ -15,6 +19,34 @@ class TestLesson3 extends BaseTest
 
 	public static function run()
 	{
-		static::registerError('Test');
+		static::testTemplates();
+		// Только шаблоны .default, inner, main
+		// B_PROLOG_INCLUDED === true || die();
+		// Asset::getInstance()->add
+		// <?php
+		// Плагины jquery для слайдера и для карусели подключить только в шаблоне главной страницы
+		// переводы
+		// GetMessage заменяй на аналог в новом ядре
+	}
+
+	protected static function testTemplates()
+	{
+		$templatesAllowed = [
+			'main',
+			'inner',
+			'.default',
+		];
+
+		$templatesDirectory = new Directory(Application::getDocumentRoot() . '/local/templates/');
+		foreach ($templatesDirectory->getChildren() as $child) {
+			if ($child->isDirectory()) {
+				if (!in_array($child->getName(), $templatesAllowed)) {
+					static::registerError(Loc::getMessage('INTERVOLGA_EDU.UNKNOWN_TEMPLATE_FOUND', [
+						'#NAME#' => $child->getName(),
+						'#ADMIN_LINK#' => Admin::getFileManUrl($child),
+					]));
+				}
+			}
+		}
 	}
 }
