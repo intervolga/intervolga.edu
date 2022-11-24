e17ae4de7e1f090127e08a7ebd41be2d6cfacc1a
erofeev@intervolga.ru <erofeev@intervolga.ru>
2022-11-24 21:40:31 +0300

    Модуль переименован

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -1,5 +1,5 @@
 <?php
-$MESS['INTERVOLGA_EDU.MODULE_NAME'] = 'Интерволга: помощник для Академии 1С-Битрикс: Управление сайтом';
+$MESS['INTERVOLGA_EDU.MODULE_NAME'] = 'Проверка Академии';
 $MESS['INTERVOLGA_EDU.MODULE_DESC'] = 'Набор автоматических проверок заданий Академии 1С-Битрикс: Управление сайтом';
 $MESS['INTERVOLGA_EDU.PARTNER_NAME'] = 'ИНТЕРВОЛГА';
 $MESS['INTERVOLGA_EDU.PARTNER_URI'] = 'http://www.intervolga.ru';
