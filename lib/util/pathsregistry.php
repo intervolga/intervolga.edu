548cede98b9b3ad304469e215769e6dd7446f679
erofeev <erofeev@intervolga.ru>
2022-11-29 15:32:54 +0300

    Добавлен класс PathsRegistry

---
+++ lib/util/pathsregistry.php
@@ -0,0 +1,33 @@
+<?php
+namespace Intervolga\Edu\Util;
+
+use Bitrix\Main\Application;
+use Bitrix\Main\IO\Directory;
+
+class PathsRegistry
+{
+	const NON_PUBLIC_DIRS = [
+		'/upload/',
+		'/bitrix/',
+		'/local/',
+	];
+
+	/**
+	 * @return Directory[]
+	 * @throws \Bitrix\Main\IO\FileNotFoundException
+	 */
+	public static function getPublicDirsLevelOne(): array
+	{
+		$result = [];
+		$directory = new Directory(Application::getDocumentRoot());
+		foreach ($directory->getChildren() as $child) {
+			if ($child->isDirectory()) {
+				if (!in_array('/' . $child->getName() . '/', static::NON_PUBLIC_DIRS)) {
+					$result[] = $child;
+				}
+			}
+		}
+
+		return $result;
+	}
+}
