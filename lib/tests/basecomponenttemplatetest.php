b32b6014abcc4c77e30f251ba29cd56d5d01574a
erofeev@intervolga.ru <erofeev@intervolga.ru>
2022-11-30 04:31:31 +0300

    Мелкие правки по замечаниям phpStorm

--- lib/tests/basecomponenttemplatetest.php
+++ lib/tests/basecomponenttemplatetest.php
@@ -8,7 +8,6 @@ use Bitrix\Main\Localization\Loc;
 use Intervolga\Edu\Util\Admin;
 use Intervolga\Edu\Util\FileSystem;
 use Intervolga\Edu\Util\Regex;
-use Intervolga\Edu\Util\Registry\Iblock\ReviewsIblock;
 use Intervolga\Edu\Util\Registry\RegexRegistry;
 
 abstract class BaseComponentTemplateTest extends BaseTest
