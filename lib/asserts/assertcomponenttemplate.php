8edce7ce3abe9ed1a44d34083c854824f4f82e7f
erofeev@intervolga.ru <erofeev@intervolga.ru>
2022-12-03 20:06:33 +0300

    Добавлен AssertComponentTemplate для простой проверки шаблонов компонентов

---
+++ lib/asserts/assertcomponenttemplate.php
@@ -0,0 +1,23 @@
+<?php
+namespace Intervolga\Edu\Asserts;
+
+use Intervolga\Edu\Util\ComponentTemplates\BaseComponentTemplate;
+
+class AssertComponentTemplate
+{
+	public static function assertNoTrashFiles(BaseComponentTemplate $template)
+	{
+
+	}
+
+	public static function assertNoUnknownFiles(BaseComponentTemplate $template)
+	{
+
+	}
+
+	public static function assertNoEnFiles(BaseComponentTemplate $template)
+	{
+
+	}
+
+}
