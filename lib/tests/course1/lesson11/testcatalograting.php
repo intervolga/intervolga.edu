bf5e50de2d26f527a3b1cfafb8cd6a7b4a671d74
whitelacoste <liliya@intervolga.ru>
2022-12-07 18:25:59 +0300

    [IVEDU-2] 3 задание

---
+++ lib/tests/course1/lesson11/testcatalograting.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Intervolga\Edu\Tests\Course1\Lesson11;
+
+use Intervolga\Edu\Asserts\Assert;
+use Intervolga\Edu\Tests\BaseTest;
+use Intervolga\Edu\Util\ComponentParameters;
+
+class TestCatalogRating extends BaseTest
+{
+	protected static function run()
+	{
+		$parameters = ComponentParameters::getComponentParameters('bitrix:catalog');
+		Assert::eq(
+			$parameters['DETAIL_USE_VOTE_RATING'],
+			'Y'
+		);
+	}
+}
