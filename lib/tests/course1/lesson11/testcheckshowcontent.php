3504709fe751eba853fdb5df1731fa49c5aa06e7
liliya <whitelacoste@mail.ru>
2022-12-08 10:47:33 +0300

    [IVEDU-7] доделала 3 задание

--- lib/tests/course1/lesson11/testcheckshowcontent.php
+++ lib/tests/course1/lesson11/testcheckshowcontent.php
@@ -4,24 +4,24 @@ namespace Intervolga\Edu\Tests\Course1\Lesson11;
 
 use Bitrix\Main\Localization\Loc;
 use Intervolga\Edu\Asserts\Assert;
-use Intervolga\Edu\Tests\BaseTestCode;
+use Intervolga\Edu\Tests\BaseTest;
 use Intervolga\Edu\Util\FileSystem;
 use Intervolga\Edu\Util\Regex;
 
-class TestCheckShowContent extends BaseTestCode
+class TestCheckShowContent extends BaseTest
 {
 	static function getFilesToTestCode(): array
 	{
 		return [FileSystem::getFile('/local/templates/inner/header.php')];
 	}
-	
+
 	protected static function run()
 	{
 		$files = static::getFilesToTestCode();
 		foreach ($files as $file)
 			Assert::fileContentMatches(
 				$file,
-				new Regex('/\<div\s*class\s*=\s*\"\s*main_title\s*\">\s*\<\?=\s*\$APPLICATION\s*->\s*ShowViewContent/i', Loc::getMessage('INTERVOLGA_EDU.SET_VIEW_TARGET'))
+				new Regex('/\<div\s*class\s*=\s*\"\s*main_title\s*\">\s*\<\?(php\s*|=\s*)\$APPLICATION\s*->\s*ShowViewContent/i', Loc::getMessage('INTERVOLGA_EDU.SHOW_VIEW_CONTENT'))
 			);
 	}
 }
