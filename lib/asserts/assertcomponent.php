e50dc89685c83f6c230838aa6b81170d19effcff
whitelacoste <liliya@intervolga.ru>
2022-12-14 15:03:12 +0300

    [IVEDU-2] вывод ошибки подправила

--- lib/asserts/assertcomponent.php
+++ lib/asserts/assertcomponent.php
@@ -10,6 +10,13 @@ class AssertComponent extends Assert
 	public static function checkComponentInTable($filter)
 	{
 		$count = ParametersTable::getCount($filter);
-		Assert::notEmpty($count, Loc::getMessage('INTERVOLGA_EDU.ASSERT_COMPONENT_NOT_FOUND'));
+		$name = static::valueToString($filter);
+		$name = substr($name, 32);
+		$name = substr($name, 0, -4);
+
+		Assert::notEmpty($count, Loc::getMessage('INTERVOLGA_EDU.ASSERT_COMPONENT_NOT_FOUND',
+			[
+				'#VALUE#' => $name,
+			]));
 	}
 }
