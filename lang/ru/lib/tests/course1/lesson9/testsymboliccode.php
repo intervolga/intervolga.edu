49eb7960ca17754ace4fe58eef408900c44db1af
whitelacoste <liliya@intervolga.ru>
2023-01-11 11:49:37 +0300

    [IVEDU-27] проверка для разделов

--- lang/ru/lib/tests/course1/lesson9/testsymboliccode.php
+++ lang/ru/lib/tests/course1/lesson9/testsymboliccode.php
@@ -1,3 +1,4 @@
 <?php
-$MESS['INTERVOLGA_EDU.COURSE_1_LESSON_1_9_CODE_IS_REQUIRED'] = 'Символьный код не является обязательным полем.';
-$MESS['INTERVOLGA_EDU.COURSE_1_LESSON_1_9_CODE_IS_EMPTY'] = 'Кол-во инфоблоков, у которых не заполнен символьный код: ';
+$MESS['INTERVOLGA_EDU.COURSE_1_LESSON_1_9_CODE_IS_REQUIRED'] = 'Символьный код должен быть обязательным полем для инфоблока "Продукция".';
+$MESS['INTERVOLGA_EDU.COURSE_1_LESSON_1_9_SECTION_CODE_IS_EMPTY'] = 'Для раздела #NAME# инфоблока "Продукция" не заполнен символьный код.';
+$MESS['INTERVOLGA_EDU.COURSE_1_LESSON_1_9_CODE_IS_EMPTY'] = 'Кол-во инфоблоков "Продукции", у которых не заполнен символьный код: ';
--- lib/tests/course1/lesson9/testsymboliccode.php
+++ lib/tests/course1/lesson9/testsymboliccode.php
@@ -4,6 +4,7 @@ namespace Intervolga\Edu\Tests\Course1\Lesson9;
 use Bitrix\Main\Localization\Loc;
 use CIBlock;
 use CIBlockElement;
+use CIBlockSection;
 use Intervolga\Edu\Asserts\Assert;
 use Intervolga\Edu\Locator\Iblock\ProductsIblock;
 use Intervolga\Edu\Tests\BaseTest;
@@ -16,6 +17,11 @@ class TestSymbolicCode extends BaseTest
 
 		$parameterCode = CIBlock::GetFields(ProductsIblock::find()['ID'])['CODE']['IS_REQUIRED'];
 		Assert::eq($parameterCode, 'Y', Loc::getMessage('INTERVOLGA_EDU.COURSE_1_LESSON_1_9_CODE_IS_REQUIRED'));
+
+		$sectionsCode = CIBlockSection::GetList(false, ['IBLOCK_ID' => ProductsIblock::find()['ID']]);
+		while ($section = $sectionsCode->fetch()) {
+			Assert::notEmpty($section['CODE'], Loc::getMessage('INTERVOLGA_EDU.COURSE_1_LESSON_1_9_SECTION_CODE_IS_EMPTY', ['#NAME#' => $section['NAME']]));
+		}
 
 		$allIblock = CIblockElement::getList(false, ['IBLOCK_ID' => ProductsIblock::find()['ID']], []);
 		$iblockWithoutCode = CIblockElement::getList(false, [
