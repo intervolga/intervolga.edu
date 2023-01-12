c6db8302a73266f162df4ec252d6f3318df930de
erofeev@intervolga.ru <erofeev@intervolga.ru>
2023-01-12 18:16:57 +0300

    [IVEDU-36] Правка Course1\Lesson3\TestOption

--- lib/tests/course1/lesson3/testoption.php
+++ lib/tests/course1/lesson3/testoption.php
@@ -1,28 +1,32 @@
 <?php
 namespace Intervolga\Edu\Tests\Course1\Lesson3;
 
+use Bitrix\Main\Application;
+use Bitrix\Main\Localization\Loc;
 use Intervolga\Edu\Asserts\Assert;
 use Intervolga\Edu\Exceptions\AssertException;
 use Intervolga\Edu\Tests\BaseTest;
 
+Loc::loadMessages(Application::getDocumentRoot() . '/bitrix/modules/main/options.php');
 
 class TestOption extends BaseTest
 {
+	public static function interceptErrors()
+	{
+		return true;
+	}
+
 	/**
 	 * @throws AssertException
 	 */
 	protected static function run()
 	{
-		$keyName = [
-			'optimize_css_files' => 'Объединять CSS файлы',
-			'optimize_js_files' => 'Объединять JS файлы',
-			'use_minified_assets' => 'Подключать минифицированные версии CSS и JS файлов',
-			'move_js_to_body' => 'Переместить весь Javascript в конец страницы',
-			'compres_css_js_files' => 'Создавать сжатую копию объединенных CSS и JS файлов'
-		];
-		foreach ($keyName as $k => $n) {
-			Assert::checkModuleOption('main', $k, 'Y', $n);
-		}
+		Assert::checkModuleOption('main', 'optimize_css_files', 'Y', Loc::getMessage('MAIN_OPTIMIZE_CSS'));
+		Assert::checkModuleOption('main', 'optimize_js_files', 'Y', Loc::getMessage('MAIN_OPTIMIZE_JS'));
+		Assert::checkModuleOption('main', 'compres_css_js_files', 'Y', Loc::getMessage('MAIN_COMPRES_CSS_JS'));
+
+		Assert::checkModuleOption('main', 'use_minified_assets', 'N', Loc::getMessage('MAIN_USE_MINIFIED_ASSETS'));
+		Assert::checkModuleOption('main', 'move_js_to_body', 'N', Loc::getMessage('MAIN_MOVE_JS_TO_BODY'));
 	}
 
 }
