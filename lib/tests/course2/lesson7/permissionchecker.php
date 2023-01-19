b54166045bb46a542bbacf6299d10811fea3bb06
liliya <whitelacoste@mail.ru>
2023-01-19 16:51:17 +0300

    [IVEDU-38] подправила

--- lib/tests/course2/lesson7/permissionchecker.php
+++ lib/tests/course2/lesson7/permissionchecker.php
@@ -3,8 +3,8 @@ namespace Intervolga\Edu\Tests\Course2\Lesson7;
 
 use Bitrix\Main\Loader;
 use Bitrix\Main\Localization\Loc;
-use \CIBlock;
-use Intervolga\Edu\Util\GroupUserList;
+use CGroup;
+use CIBlock;
 use Intervolga\Edu\Asserts\Assert;
 use Intervolga\Edu\Locator\Iblock\PromoIblock;
 use Intervolga\Edu\Tests\BaseTest;
@@ -16,9 +16,8 @@ class PermissionChecker extends BaseTest
 		Loader::includeModule('iblock');
 
 		$permission = CIBlock::GetGroupPermissions(PromoIblock::find()['ID']);
-		$groupPartnersId = key(GroupUserList::getGroupList([
-			'STRING_ID' => 'partners',
-		]));
+		$groupPartnersId = CGroup::GetList(false, false, ['STRING_ID' => 'partners'])->fetch()['ID'];
+
 		Assert::notEmpty($groupPartnersId, Loc::getMessage('INTERVOLGA_EDU.COURSE_2_LESSON_7_STRING_ID_PARTNERS'));
 		Assert::eq($permission[$groupPartnersId], 'W', Loc::getMessage('INTERVOLGA_EDU.COURSE_2_LESSON_7_GROUP_PARTNERS_PERMISSONS'));
 
