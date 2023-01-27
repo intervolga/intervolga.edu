b61e6309d6642d01189d22471bf5c379cf625322
liliya <liliya@intervolga.ru>
2023-01-27 13:06:19 +0300

    [IVEDU-52] проверка на существование файла, файл = картинка

--- lib/tests/course1/lesson2/testpartnerspage.php
+++ lib/tests/course1/lesson2/testpartnerspage.php
@@ -1,6 +1,8 @@
 <?php
 namespace Intervolga\Edu\Tests\Course1\Lesson2;
 
+use Bitrix\Main\Application;
+use Bitrix\Main\IO\File;
 use Bitrix\Main\Localization\Loc;
 use Intervolga\Edu\Asserts\Assert;
 use Intervolga\Edu\Locator\IO\PartnersSection;
@@ -25,6 +27,8 @@ class TestPartnersPage extends BaseTest
 				$indexFile,
 				new Regex('/<img/i', Loc::getMessage('INTERVOLGA_EDU.IMG_TAG'))
 			);
+			static::imgExistChecker($indexFile);
+
 			Assert::fileContentMatches(
 				$indexFile,
 				new Regex('/<table/i', Loc::getMessage('INTERVOLGA_EDU.TABLE_TAG'))
@@ -33,6 +37,27 @@ class TestPartnersPage extends BaseTest
 				$indexFile,
 				new Regex('/\/upload\//i', Loc::getMessage('INTERVOLGA_EDU.UPLOAD_PATH'))
 			);
+		}
+	}
+
+	protected static function imgExistChecker(File $indexFile)
+	{
+		$content = $indexFile->getContents();
+		if ($content) {
+			preg_match_all('/<img\s*src=\"[\w\s\/\.]*\"/i', $content, $matches, PREG_SET_ORDER);
+			$fileImg['path'] = mb_strcut($matches[0][0], mb_stripos($matches[0][0], '"') + 1, -1);
+			$img = new File(Application::getDocumentRoot() . $fileImg['path']);
+
+			Assert::true($img->isExists(), Loc::getMessage('INTERVOLGA_EDU.COURSE_1_LESSON_2_FILE_NOT_EXIST',
+				['#FILE_PATH#' => $fileImg['path']]));
+
+			if ($img->isExists()) {
+				Assert::matches($fileImg['type'], new Regex('/image/i', ''),
+					Loc::getMessage('INTERVOLGA_EDU.COURSE_1_LESSON_2_FILE_NOT_IMG', [
+						'#FILE_PATH#' => $fileImg['path'],
+						'#FILE_TYPE#' => $fileImg['type']
+					]));
+			}
 		}
 	}
 }
