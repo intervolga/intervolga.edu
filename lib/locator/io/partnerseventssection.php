678ed96012b17e628176456aa4fddde91ef70aaf
erofeev@intervolga.ru <erofeev@intervolga.ru>
2023-01-30 22:17:01 +0300

    [IVDEV-33] Добавлен класс локатора расписания для партнеров

---
+++ lib/locator/io/partnerseventssection.php
@@ -0,0 +1,27 @@
+<?php
+namespace Intervolga\Edu\Locator\IO;
+
+use Bitrix\Main\Localization\Loc;
+
+class PartnersEventsSection extends DirectoryLocator
+{
+	protected static function getRootLocator()
+	{
+		return PartnersSection::class;
+	}
+
+	protected static function getPaths(): array
+	{
+		return [
+			'/event/',
+			'/events/',
+			'/schedule-of-event/',
+			'/schedule-of-events/'
+		];
+	}
+
+	public static function getNameLoc(): string
+	{
+		return Loc::getMessage('INTERVOLGA_EDU.PARTNERS_EVENTS_DIRECTORY');
+	}
+}
