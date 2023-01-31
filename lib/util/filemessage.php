4ac62ca19203b5a7758c9dccd162ef20832a502e
liliya <liliya@intervolga.ru>
2023-01-31 11:58:34 +0300

    [IVEDU-53] маленькие правки, вернула старый fse, чтобы не ломался getPossibleTips

--- lib/util/filemessage.php
+++ lib/util/filemessage.php
@@ -3,9 +3,8 @@ namespace Intervolga\Edu\Util;
 
 class FileMessage
 {
-	public static function getFileMessage($replace = null):string{
+	public static function getFileMessage($replace = ['#FULL_PATH#'=>'','#NAME#'=>'','#FILEMAN_URL#'=>'']):string{
 		$message = '#FULL_PATH#<a href="#FILEMAN_URL#" target="_blank" id="#FULL_PATH##NAME#" ><b>#NAME#</b></a> <button title="копировать путь для PhpStorm" class="iv-copy-link"></button>';
-
 		if (is_array($replace))
 		{
 			$message = strtr($message, $replace);
