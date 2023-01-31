130aaa0fc9b80d8d7b8c53c917fca133f5953048
erofeev@intervolga.ru <erofeev@intervolga.ru>
2023-01-31 20:24:17 +0300

    [IVEDU-27] Правки lang-строк, мелкие правки логики теста

--- lang/ru/lib/tests/course1/lesson9/testnavpage.php
+++
@@ -1,4 +0,0 @@
-<?php
-$MESS['INTERVOLGA_EDU.COURSE_1_LESSON_1_9_PAGE_ELEMENT_COUNT'] = 'Количество элементов на странице должно быть равно пяти.';
-$MESS['INTERVOLGA_EDU.COURSE_1_LESSON_1_9_PAGER_TEMPLATE'] = 'У постраничной навигации должен быть выбран шаблон arrows';
-$MESS['INTERVOLGA_EDU.COURSE_1_LESSON_1_9_DISPLAY_PAGER'] = 'Не установлен вывод постраничной навигации (выводить над/под списком).';
--- lib/tests/course1/lesson9/testnavpage.php
+++ lib/tests/course1/lesson9/testnavpage.php
@@ -1,22 +1,41 @@
 <?php
 namespace Intervolga\Edu\Tests\Course1\Lesson9;
 
-use Bitrix\Main\Localization\Loc;
-use Intervolga\Edu\Asserts\Assert;
+use Intervolga\Edu\Asserts\AssertComponent;
 use Intervolga\Edu\Locator\Component\Catalog;
 use Intervolga\Edu\Tests\BaseTest;
 
 class TestNavPage extends BaseTest
 {
+	public static function interceptErrors()
+	{
+		return true;
+	}
+
 	protected static function run()
 	{
-		$parameters = Catalog::find()['PARAMETERS'];
-
-		Assert::eq($parameters['PAGE_ELEMENT_COUNT'], 5, Loc::getMessage('INTERVOLGA_EDU.COURSE_1_LESSON_1_9_PAGE_ELEMENT_COUNT'));
-		Assert::eq($parameters['PAGER_TEMPLATE'], 'arrows', Loc::getMessage('INTERVOLGA_EDU.COURSE_1_LESSON_1_9_PAGER_TEMPLATE'));
-		if ($parameters['DISPLAY_TOP_PAGER'] != 'Y') {
-			Assert::eq($parameters['DISPLAY_BOTTOM_PAGER'], 'Y', Loc::getMessage('INTERVOLGA_EDU.COURSE_1_LESSON_1_9_DISPLAY_PAGER'));
+		AssertComponent::componentLocator(Catalog::class);
+		if (Catalog::find()) {
+			AssertComponent::parameterEq(
+				Catalog::class,
+				'PAGE_ELEMENT_COUNT',
+				5
+			);
+			AssertComponent::parameterEq(
+				Catalog::class,
+				'PAGER_TEMPLATE',
+				'arrows'
+			);
+			AssertComponent::parameterEq(
+				Catalog::class,
+				'DISPLAY_TOP_PAGER',
+				'N'
+			);
+			AssertComponent::parameterEq(
+				Catalog::class,
+				'DISPLAY_BOTTOM_PAGER',
+				'Y'
+			);
 		}
-
 	}
 }
