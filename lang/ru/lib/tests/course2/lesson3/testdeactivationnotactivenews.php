09b1c4312b531792e4c4ebb0fb4b8f956d77faee
erofeev@intervolga.ru <erofeev@intervolga.ru>
2023-02-01 22:37:55 +0300

    [IVEDU-42] Оформление кода

--- lang/ru/lib/tests/course2/lesson3/testdeactivationnotactivenews.php
+++ lang/ru/lib/tests/course2/lesson3/testdeactivationnotactivenews.php
@@ -1,3 +1,3 @@
 <?php
-$MESS['INTERVOLGA_EDU.COURSE2_LESSON3_CREATE_NOT_ACTIVE_IB_FAILED']= 'Не удалось создать деактивированную новость';
-$MESS['INTERVOLGA_EDU.COURSE2_LESSON3_NOT_ACTIVE_NEWS_DEACTIVATED']= 'Произошла ошибка при деактивации уже деактивированной новости';
+$MESS['INTERVOLGA_EDU.COURSE2_LESSON3_CREATE_NOT_ACTIVE_IB_FAILED'] = 'Не удалось создать деактивированную новость';
+$MESS['INTERVOLGA_EDU.COURSE2_LESSON3_NOT_ACTIVE_NEWS_DEACTIVATED'] = 'Произошла ошибка при деактивации уже деактивированной новости';
