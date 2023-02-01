b606c9b075b731151de9830c302e40428beae167
liliya <liliya@intervolga.ru>
2023-02-01 11:21:06 +0300

    [IVEDU-53] исправления языковых

--- lang/ru/lib/asserts/assert.php
+++ lang/ru/lib/asserts/assert.php
@@ -15,13 +15,13 @@ $MESS['INTERVOLGA_EDU.ASSERT_FUNCTION_EXISTS'] = 'Функция #VALUE# дол�
 $MESS['INTERVOLGA_EDU.ASSERT_FSE_EXISTS'] = 'Файл #VALUE# не найден';
 $MESS['INTERVOLGA_EDU.ASSERT_FSE_NOT_EXISTS'] = '#VALUE# требуется удалить';
 $MESS['INTERVOLGA_EDU.ASSERT_FILE_IS_IMAGE'] = 'Файл #VALUE# должен быть картинкой';
-$MESS['INTERVOLGA_EDU.ASSERT_FSE_NAME_MATCH'] = 'Имя файла должно иметь вид <b>#EXPECT#</b>, сейчас #VALUE#</>';
-$MESS['INTERVOLGA_EDU.ASSERT_FILE_CONTENT_NOT_MATCH'] = '<>Файл #VALUE# содержит <b>#EXPECT#</b>';
+$MESS['INTERVOLGA_EDU.ASSERT_FSE_NAME_MATCH'] = 'Имя файла должно иметь вид <b>#EXPECT#</b>, сейчас #VALUE#';
+$MESS['INTERVOLGA_EDU.ASSERT_FILE_CONTENT_NOT_MATCH'] = 'Файл #VALUE# содержит <b>#EXPECT#</b>';
 $MESS['INTERVOLGA_EDU.ASSERT_FILE_CONTENT_MATCH'] = 'Файл #VALUE# не содержит <b>#EXPECT#</b>';
 $MESS['INTERVOLGA_EDU.ASSERT_DIRECTORY_EXISTS'] = 'Папка #VALUE# не найдена';
 $MESS['INTERVOLGA_EDU.ASSERT_DIRECTORY_NOT_EXISTS'] = 'Папка #VALUE# существует, должна быть удалена';
 $MESS['INTERVOLGA_EDU.ASSERT_FILE_NOT_EXISTS'] = 'Файл #VALUE# существует, должен быть удален';
-$MESS['INTERVOLGA_EDU.FSE'] = '<a href="#FILEMAN_URL#" target="_blank"><b>#NAME#</b></a>';
+$MESS['INTERVOLGA_EDU.FSE'] = '<a href="#FILEMAN_URL#" target="_blank">#NAME#</a>';
 $MESS['INTERVOLGA_EDU.ASSERT_MODULE_INSTALLED'] = 'Модуль #VALUE# должен быть установлен';
 $MESS['INTERVOLGA_EDU.ASSERT_IBLOCK_LOCATOR'] = 'Инфоблок <b>#IBLOCK#</b> не найден (#POSSIBLE#)';
 $MESS['INTERVOLGA_EDU.ASSERT_SECTION_LOCATOR'] = 'Раздел <b>#SECTION#</b> инфоблока <b>#IBLOCK#</b> не найден (#POSSIBLE#)';
