3ae8b630099e94919aacb5237ca8fdbae66b32ae
erofeev@intervolga.ru <erofeev@intervolga.ru>
2023-02-02 23:20:13 +0300

    [IVDEV-31] Обновлен тест фукнции dump

--- lang/ru/lib/tests/course1/lesson2/testdumpfunction.php
+++
@@ -1,2 +0,0 @@
-<?php
-$MESS['INTERVOLGA_EDU.NOT_FOUND_DUMP'] = 'Не найдена собственная функция. Возможные варианты: #POSSIBLE#';
--- lib/tests/course1/lesson2/testdumpfunction.php
+++ lib/tests/course1/lesson2/testdumpfunction.php
@@ -1,24 +1,14 @@
 <?php
 namespace Intervolga\Edu\Tests\Course1\Lesson2;
 
-use Bitrix\Main\Localization\Loc;
 use Intervolga\Edu\Asserts\Assert;
-use Intervolga\Edu\Locator\IO\DumpFunction;
+use Intervolga\Edu\Locator\DumpFunction;
 use Intervolga\Edu\Tests\BaseTest;
 
 class TestDumpFunction extends BaseTest
 {
 	protected static function run()
 	{
-		//файл с функцией существует
-		Assert::fileLocator(DumpFunction::class);
-		//функция существует
-		Assert::functionExists(DumpFunction::findFunctionDump(), Loc::getMessage('INTERVOLGA_EDU.NOT_FOUND_DUMP',
-			[
-				'#POSSIBLE#' => implode(' || ',  DumpFunction::getPossibleName())
-			]
-		));
+		Assert::functionExists(DumpFunction::class);
 	}
-
-
 }
