128680e68c0c5d0fb8f61b3f4ee8b9206f4c529d
liliya <liliya@intervolga.ru>
2023-02-03 14:32:21 +0300

    [IVEDU-34] потеряшка

--- lang/ru/lib/sniffer/standards/general/Sniffs/php/CheckCustomCoreSniff.php
+++ lang/ru/lib/sniffer/standards/general/Sniffs/php/CheckCustomCoreSniff.php
@@ -1,3 +1,3 @@
 <?php
-$MESS['INTERVOLGA_EDU.SNIFFER_CUSTOM_CORE'] = '<div>В файле #FILE# используйте <b>B_PROLOG_INCLUDED === true || die();</b></div>';
-$MESS['INTERVOLGA_EDU.SNIFFER_CUSTOM_CORE_NOT_FOUND'] = '<div>В файле #FILE# отсутствует подключение ядра (B_PROLOG_INCLUDED);</b></div>';
+$MESS['INTERVOLGA_EDU.SNIFFER_CUSTOM_CORE'] = 'В файле #FILE# используйте <b>B_PROLOG_INCLUDED === true || die();</b>';
+$MESS['INTERVOLGA_EDU.SNIFFER_CUSTOM_CORE_NOT_FOUND'] = 'В файле #FILE# отсутствует подключение ядра (B_PROLOG_INCLUDED);</b>';
