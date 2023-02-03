9a87a49857af33e5d23be8d1855ff40ccdb36b3e
liliya <liliya@intervolga.ru>
2023-02-03 16:08:10 +0300

    [IVEDU-57] мелкие исправления + новый вывод

--- lib/tests/course2/lesson5_2/testcomponentcache.php
+++ lib/tests/course2/lesson5_2/testcomponentcache.php
@@ -7,9 +7,7 @@ use Intervolga\Edu\Asserts\Assert;
 use Intervolga\Edu\Locator\IO\CustomComponent;
 use Intervolga\Edu\Sniffer;
 use Intervolga\Edu\Tests\BaseTest;
-use Intervolga\Edu\Util\Admin;
 use Intervolga\Edu\Util\FileMessage;
-use Intervolga\Edu\Util\FileSystem;
 
 class TestComponentCache extends BaseTest
 {
@@ -27,32 +25,32 @@ class TestComponentCache extends BaseTest
 		foreach ($cacheComponent as $component) {
 			$foundCache .= $component->getMessage();
 		}
-		Assert::notEmpty(strpos($foundCache, 'CACHE_TIME'), Loc::getMessage('INTERVOLGA_EDU.COURSE_2_LESSON_5_2_NOT_FOUND_USAGE_CACHE_PARAMETER', [
-			'#FILE#' => FileMessage::getFileMessage([
-				'#FULL_PATH#' => str_replace($classFile->getName(), '', FileSystem::getLocalPath($classFile)),
-				'#NAME#' => $classFile->getName(),
-				'#FILEMAN_URL#' => Admin::getFileManUrl($classFile),
-			])
-		]));
-		Assert::notEmpty(strpos($foundCache, 'StartResultCache'), Loc::getMessage('INTERVOLGA_EDU.COURSE_2_LESSON_5_2_NOT_FOUND_START_RESULT_CACHE', [
-			'#FILE#' => FileMessage::getFileMessage([
-				'#FULL_PATH#' => str_replace($classFile->getName(), '', FileSystem::getLocalPath($classFile)),
-				'#NAME#' => $classFile->getName(),
-				'#FILEMAN_URL#' => Admin::getFileManUrl($classFile),
-			])
-		]));
+		Assert::notEmpty(strpos($foundCache, 'CACHE_TIME'),
+			Loc::getMessage('INTERVOLGA_EDU.COURSE_2_LESSON_5_2_NOT_FOUND_USAGE_CACHE_PARAMETER',
+				[
+					'#FILE#' => FileMessage::get($classFile)
+				]
+			)
+		);
+		Assert::notEmpty(strpos($foundCache, 'StartResultCache'),
+			Loc::getMessage('INTERVOLGA_EDU.COURSE_2_LESSON_5_2_NOT_FOUND_START_RESULT_CACHE',
+				[
+					'#FILE#' => FileMessage::get($classFile)
+				]
+			)
+		);
 	}
 
 	protected static function cacheInComponentParameters()
 	{
 		$parametersFile = CustomComponent::getParametersFilePath();
 		include Application::getDocumentRoot() . $parametersFile->getPath();
-		Assert::notEmpty($arComponentParameters['PARAMETERS']['CACHE_TIME'], Loc::getMessage('INTERVOLGA_EDU.COURSE_2_LESSON_5_2_NOT_FOUND_CACHE_TIME_IN_PARAMETERS', [
-			'#FILE#' => FileMessage::getFileMessage([
-				'#FULL_PATH#' => str_replace($parametersFile->getName(), '', FileSystem::getLocalPath($parametersFile)),
-				'#NAME#' => $parametersFile->getName(),
-				'#FILEMAN_URL#' => Admin::getFileManUrl($parametersFile),
-			])
-		]));
+		Assert::notEmpty($arComponentParameters['PARAMETERS']['CACHE_TIME'],
+			Loc::getMessage('INTERVOLGA_EDU.COURSE_2_LESSON_5_2_NOT_FOUND_CACHE_TIME_IN_PARAMETERS',
+				[
+					'#FILE#' => FileMessage::get($parametersFile)
+				]
+			)
+		);
 	}
 }
