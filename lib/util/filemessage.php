d5ff8703f7a19f35036958b9a515318c6323a5aa
erofeev@intervolga.ru <erofeev@intervolga.ru>
2023-02-06 20:26:12 +0300

    [IVEDU-34] Дополнена подсказка для файлов в сообщениях (копируется номер строки и файла)

--- lib/util/filemessage.php
+++ lib/util/filemessage.php
@@ -8,19 +8,29 @@ class FileMessage
 {
 	public static function get(FileSystemEntry $fse, int $line = 0, int $column = 0): string
 	{
-		$fullPath = FileSystem::getLocalPath($fse);
+		$coords = '';
 		if ($line) {
-			$fullPath .= ':' . $line;
 			if ($column) {
-				$fullPath .= ':' . $column;
+				$coords = Loc::getMessage('INTERVOLGA_EDU.FSE_MESSAGE_COORDS_LINE_COLUMN', [
+					'#LINE#' => $line,
+					'#COLUMN#' => $column,
+				]);
+			}
+			else
+			{
+				$coords = Loc::getMessage('INTERVOLGA_EDU.FSE_MESSAGE_COORDS_LINE', [
+					'#LINE#' => $line,
+				]);
 			}
 		}
+		$fullPath = FileSystem::getLocalPath($fse);
 		$replace = [
 			'#FULL_PATH#' => $fullPath,
 			'#PATH_START#' => FileSystem::getLocalPath($fse->getDirectory()),
 			'#NAME#' => $fse->getName(),
 			'#PATH_END#' => $fse->isDirectory() ? '/' : '',
 			'#FILEMAN_URL#' => Admin::getFileManUrl($fse),
+			'#COORDS#' => $coords,
 		];
 		$message = Loc::getMessage('INTERVOLGA_EDU.FSE_MESSAGE', $replace);
 
