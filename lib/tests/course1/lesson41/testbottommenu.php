6090f30e6c6e7b57bceb169bc0d98ca96e78ccbf
chernikov <chernikov@intervolga.ru>
2023-02-06 17:33:11 +0300

    [IVEDU-70] - Доработка 1-4.1              Проверять состав пунктов меню "О магазине"              Отзывы, Контакты, Руководство, История

--- lib/tests/course1/lesson41/testbottommenu.php
+++ lib/tests/course1/lesson41/testbottommenu.php
@@ -1,6 +1,7 @@
 <?php
 namespace Intervolga\Edu\Tests\Course1\Lesson41;
 
+use Intervolga\Edu\Asserts\Assert;
 use Intervolga\Edu\FilesTree\ComponentTemplate;
 use Intervolga\Edu\FilesTree\SimpleComponentTemplate;
 use Intervolga\Edu\Locator\IO\ComponentTemplate\BottomMenuTemplate;
@@ -9,6 +10,17 @@ use Intervolga\Edu\Tests\BaseComponentTemplateTest;
 
 class TestBottomMenu extends BaseComponentTemplateTest
 {
+
+	protected static function run()
+	{
+		parent::run();
+		$fields = ['/company/reviews/', '/contacts/', '/company/management/', '/company/history/'];
+
+		foreach ($fields as $field) {
+			Assert::menuItemExists('/.bottom.menu.php', $field);
+		}
+	}
+
 	/**
 	 * @return string|DirectoryLocator
 	 */
