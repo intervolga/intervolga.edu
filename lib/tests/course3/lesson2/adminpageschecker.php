12d8ab7d3772bc84688135efa58480257ef0f7b3
liliya <liliya@intervolga.ru>
2023-02-08 18:01:28 +0300

    [IVEDU-61] проверка админ страниц + страниц их подключения

--- lib/tests/course3/lesson2/adminpageschecker.php
+++ lib/tests/course3/lesson2/adminpageschecker.php
@@ -1,26 +1,23 @@
 <?php
 namespace Intervolga\Edu\Tests\Course3\Lesson2;
 
-use Intervolga\Edu\FilesTree\ComponentTemplate;
-use Intervolga\Edu\FilesTree\CustomModuleTree;
-use Intervolga\Edu\Locator\IO\CustomModule;
-use Intervolga\Edu\Locator\IO\DirectoryLocator;
-use Intervolga\Edu\Tests\BaseCustomModuleTest;
+use Intervolga\Edu\Asserts\Assert;
+use Intervolga\Edu\Locator\Module\AdminFiles\EditFile;
+use Intervolga\Edu\Locator\Module\AdminFiles\MenuFile;
+use Intervolga\Edu\Locator\Module\AdminFiles\TableFile;
+use Intervolga\Edu\Locator\Module\InstallFiles\Admin\AdminEditFile;
+use Intervolga\Edu\Locator\Module\InstallFiles\Admin\AdminTableFile;
+use Intervolga\Edu\Tests\BaseTest;
 
-class AdminPagesChecker extends BaseCustomModuleTest
+class AdminPagesChecker extends BaseTest
 {
-	/**
-	 * @return string|DirectoryLocator
-	 */
-	protected static function getLocator()
+	protected static function run()
 	{
-		return CustomModule::class;
-	}
-	/**
-	 * @return string|ComponentTemplate
-	 */
-	protected static function getComponentTemplateTree()
-	{
-		return CustomModuleTree::class;
+		Assert::fileLocator(EditFile::class);
+		Assert::fileLocator(MenuFile::class);
+		Assert::fileLocator(TableFile::class);
+
+		Assert::fileLocator(AdminEditFile::class);
+		Assert::fileLocator(AdminTableFile::class);
 	}
 }
