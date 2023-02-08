ce3a4509f270367685b5a56f06efcffa505e90a5
erofeev@intervolga.ru <erofeev@intervolga.ru>
2023-02-08 18:12:11 +0300

    [IVEDU-79] Добавлена проверка отправителя писем

---
+++ lang/ru/lib/tests/course2/lesson2/testpostevent.php
@@ -0,0 +1,2 @@
+<?php
+$MESS['INTERVOLGA_EDU.EMAIL_FROM_INVALID'] = 'В поле "От кого" почтового шаблона должен стоять макрос <b>#DEFAULT_EMAIL_FROM#</b>, сейчас #VALUE#';
--- lib/tests/course2/lesson2/testpostevent.php
+++ lib/tests/course2/lesson2/testpostevent.php
@@ -1,6 +1,7 @@
 <?php
 namespace Intervolga\Edu\Tests\Course2\Lesson2;
 
+use Bitrix\Main\Localization\Loc;
 use Intervolga\Edu\Asserts\Assert;
 use Intervolga\Edu\Locator\Event\Type\CheckOldStocksType;
 use Intervolga\Edu\Locator\Event\Template\CheckOldStocksTemplate;
@@ -12,5 +13,7 @@ class TestPostEvent extends BaseTest
 	{
 		Assert::eventMessageExists(CheckOldStocksType::class);
 		Assert::eventTemplateExists(CheckOldStocksTemplate::class);
+		$template = CheckOldStocksTemplate::find();
+		Assert::eq($template['EMAIL_FROM'], '#DEFAULT_EMAIL_FROM#', Loc::getMessage('INTERVOLGA_EDU.EMAIL_FROM_INVALID'));
 	}
 }
