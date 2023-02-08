73cb390fcb5a5703a1957420abd43cf75a73b4fd
liliya <liliya@intervolga.ru>
2023-02-08 15:40:53 +0300

    [IVEDU-61] проверка /local/modules/intervolga.module

---
+++ lib/filestree/custommoduletree.php
@@ -0,0 +1,70 @@
+<?php
+namespace Intervolga\Edu\FilesTree;
+
+use Bitrix\Main\IO\Directory;
+use Bitrix\Main\IO\File;
+use Bitrix\Main\IO\FileNotFoundException;
+use Intervolga\Edu\Util\FileSystem;
+
+class CustomModuleTree extends ComponentTemplate
+{
+	public function getAdminDir(): Directory
+	{
+		return FileSystem::getInnerDirectory($this, 'admin');
+	}
+
+	public function getClassesDir(): Directory
+	{
+		return FileSystem::getInnerDirectory($this, 'classes');
+	}
+
+	public function getInstallDir(): Directory
+	{
+		return FileSystem::getInnerDirectory($this, 'install');
+	}
+
+	public function getLibDir(): Directory
+	{
+		return FileSystem::getInnerDirectory($this, 'lib');
+	}
+
+	public function getIncludeFile(): File
+	{
+		return FileSystem::getInnerFile($this, 'include.php');
+	}
+
+	public function getPrologFile(): File
+	{
+		return FileSystem::getInnerFile($this, 'prolog.php');
+	}
+
+	/**
+	 * @return File[]
+	 * @throws FileNotFoundException
+	 */
+	public function getKnownFiles(): array
+	{
+		$result = [
+			$this->getIncludeFile(),
+			$this->getPrologFile(),
+		];
+
+		return $result;
+	}
+
+	/**
+	 * @return Directory[]
+	 */
+	public function getKnownDirs(): array
+	{
+		$result = [
+			$this->getAdminDir(),
+			$this->getClassesDir(),
+			$this->getInstallDir(),
+			$this->getLibDir(),
+		];
+
+		return $result;
+	}
+
+}
--- lib/tester.php
+++ lib/tester.php
@@ -109,6 +109,8 @@ class Tester
 
 			\Intervolga\Edu\Tests\Course3\Lesson1\TestModule::class,
 
+			\Intervolga\Edu\Tests\Course3\Lesson2\AdminPagesChecker::class,
+
 			\Intervolga\Edu\Tests\Course3\Lesson3\TestResultsPollingIblock::class,
 			\Intervolga\Edu\Tests\Course3\Lesson3\TestPropertyGenderValues::class,
 			\Intervolga\Edu\Tests\Course3\Lesson3\TestLinkWithRespondent::class,
