e0fbaa195aa672dc57ade538d35604dcf4210184
liliya <liliya@intervolga.ru>
2023-02-09 15:32:41 +0300

    [IVEDU-61] проверка содержимого файлов

--- lib/tester.php
+++ lib/tester.php
@@ -112,6 +112,7 @@ class Tester
 			\Intervolga\Edu\Tests\Course3\Lesson2\AdminPagesChecker::class,
 			\Intervolga\Edu\Tests\Course3\Lesson2\CustomModuleChecker::class,
 			\Intervolga\Edu\Tests\Course3\Lesson2\ClassesChecker::class,
+			\Intervolga\Edu\Tests\Course3\Lesson2\ContentPageChecker::class,
 
 			\Intervolga\Edu\Tests\Course3\Lesson3\TestResultsPollingIblock::class,
 			\Intervolga\Edu\Tests\Course3\Lesson3\TestPropertyGenderValues::class,
