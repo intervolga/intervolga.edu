74f9f71fc9772b0c3eb9393cf16fbaad340267f6
liliya <whitelacoste@mail.ru>
2023-02-27 16:23:08 +0300

    [IVEDU-60] Проверка записи в журнале событий

--- lib/tests/course2/lesson9/wizardchecker.php
+++ lib/tests/course2/lesson9/wizardchecker.php
@@ -1,6 +1,8 @@
 <?php
 namespace Intervolga\Edu\Tests\Course2\Lesson9;
 
+use Bitrix\Main\Localization\Loc;
+use CEventLog;
 use Intervolga\Edu\Asserts\Assert;
 use Intervolga\Edu\Locator\Wizard\Calculator;
 use Intervolga\Edu\Tests\BaseTest;
@@ -10,5 +12,13 @@ class WizardChecker extends BaseTest
 	protected static function run()
 	{
 		Assert::WizardLocator(Calculator::class);
+
+		$eventList = CEventLog::GetList(false,
+			[
+				'SEVERITY' => 'INFO',
+				'AUDIT_TYPE_ID' => 'intervolga:calculator'
+			]
+		)->Fetch();
+		Assert::notEmpty($eventList, Loc::getMessage('INTERVOLGA_EDU.COURSE_2_LESSON_9_MASTER_EVENT_LOG_NOT_FOUND'));
 	}
 }
