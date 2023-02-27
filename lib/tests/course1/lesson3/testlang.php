e73dfafcc0f954833bf07067fbe9ebfea348fda2
liliya <whitelacoste@mail.ru>
2023-02-27 15:47:01 +0300

    [IVEDU-35] Проверка языковых констант

--- lib/tests/course1/lesson3/testlang.php
+++ lib/tests/course1/lesson3/testlang.php
@@ -19,22 +19,25 @@ class TestLang extends BaseTest
 	const FOOTER = 'footer.php';
 	const HEADER = 'header.php';
 	const LANG_DIR = '/local/templates/.default/include/lang/';
-	const INCLUDE_DIR = '/local/templates/.default/include/lang/';
-
-	public static function interceptErrors()
-	{
-		return true;
-	}
+	const INCLUDE_DIR = '/local/templates/.default/include/';
+	const NEEDED_STRING_FOOTER = [
+		'CONTACT_INFO',
+		'WORKING_TIME',
+		'COPYRIGHT_INFO',
+	];
+	const NEEDED_STRING_HEADER = [
+		'WORKING_TIME',
+	];
 
 	protected static function checkLangFileDiscrepancy($fileName)
 	{
 
-		$ruStrings = static::getMessageLang($fileName, static::RU);
-		$enStrings = static::getMessageLang($fileName, static::EN);
+		$ruStrings = static::getMessageLang($fileName, static::LANG_DIR, static::LANG_DEFINITION, static::RU);
+		$enStrings = static::getMessageLang($fileName, static::LANG_DIR, static::LANG_DEFINITION, static::EN);
 
 		foreach ($ruStrings as $ru) {
 			Assert::true(in_array($ru, $enStrings),
-				Loc::getMessage('INTERVOLGA_EDU.COURSE_1_LESSON_3_NOT_FOUND_STRING_EN',
+				Loc::getMessage('INTERVOLGA_EDU.COURSE_1_LESSON_3_NOT_FOUND_USAGE_STRING',
 					[
 						'#FILE#' => FileMessage::get(
 							new File(Application::getDocumentRoot() . static::LANG_DIR . 'en/' . $fileName)
@@ -46,10 +49,10 @@ class TestLang extends BaseTest
 		}
 	}
 
-	protected static function getMessageLang(string $fileName, string $lang, string $standart)
+	protected static function getMessageLang(string $fileName, string $directory, string $standart, string $lang = '')
 	{
 		$result = [];
-		$path = Application::getDocumentRoot() . static::LANG_DIR . $lang . '/' . $fileName;
+		$path = Application::getDocumentRoot() . $directory . $lang . '/' . $fileName;
 		$messages = Sniffer::run([$path], [$standart]);
 		foreach ($messages as $message) {
 			$result[] = mb_strcut($message->getMessage(), 1, -1);
@@ -60,38 +63,59 @@ class TestLang extends BaseTest
 
 	protected static function run()
 	{
+		static::check(static::HEADER, static::NEEDED_STRING_HEADER);
+		static::check(static::FOOTER, static::NEEDED_STRING_FOOTER);
+	}
 
-		static::checkLangFileDiscrepancy(static::HEADER);
-		static::checkLangFileDiscrepancy(static::FOOTER);
+	protected static function check($filename, $neededString)
+	{
+		static::stringChecker($filename, $neededString);
+		static::usageCheck($filename);
+		static::checkLangFileDiscrepancy($filename);
+	}
+
+	protected static function stringChecker($fileName, $neededString)
+	{
+		$strings = static::getMessageLang($fileName, static::LANG_DIR, static::LANG_DEFINITION, static::RU);
+		foreach ($strings as $string) {
+			Assert::true(in_array($string, $neededString),
+				Loc::getMessage('INTERVOLGA_EDU.COURSE_1_LESSON_3_UNKNOWN_STRING',
+					[
+						'#FILE#' => FileMessage::get(
+							new File(Application::getDocumentRoot() . static::LANG_DIR . 'ru/' . $fileName)
+						),
+						'#VALUE#' => $string
+					]
+				)
+			);
+		}
+		Assert::empty($diff = array_diff($neededString, $strings),
+			Loc::getMessage('INTERVOLGA_EDU.COURSE_1_LESSON_3_NOT_FOUND_STRING',
+				[
+					'#FILE#' => FileMessage::get(
+						new File(Application::getDocumentRoot() . static::LANG_DIR . 'ru/' . $fileName)
+					),
+					'#VALUE#' => implode(' || ', $diff)
+				]
+			)
+		);
 	}
 
 	protected static function usageCheck($fileName)
 	{
-		$usage = static::getMessageLang($fileName, static::RU, static::LANG_USAGE);
-		$strings = static::getMessageLang($fileName, static::RU, static::LANG_DEFINITION);
+		$usage = static::getMessageLang($fileName, static::INCLUDE_DIR, static::LANG_USAGE);
+		$strings = static::getMessageLang($fileName, static::LANG_DIR, static::LANG_DEFINITION, static::RU);
 		foreach ($strings as $message) {
 			Assert::true(in_array($message, $usage),
-				Loc::getMessage('INTERVOLGA_EDU.COURSE_1_LESSON_3_NOT_FOUND_USAGE_STRING',//в файле ** не используется **
+				Loc::getMessage('INTERVOLGA_EDU.COURSE_1_LESSON_3_NOT_FOUND_USAGE_STRING',
 					[
 						'#FILE#' => FileMessage::get(
-							new File(Application::getDocumentRoot() . static::LANG_DIR . 'en/' . $fileName)
+							new File(Application::getDocumentRoot() . static::INCLUDE_DIR . $fileName)
 						),
 						'#VALUE#' => $message
 					]
 				)
 			);
 		}
-	}
-
-	protected static function getLangArrayTemplateDir(ComponentTemplate $templateDir)
-	{
-		foreach ($templateDir->getKnownFiles() as $template) {
-			$result = Sniffer::run([$template->getPath()], ['langUsage']);
-			foreach ($result as $message) {
-				$newTest[] = mb_strcut($message->getMessage(), 1, -1);
-			}
-		}
-
-		return $newTest;
 	}
 }
