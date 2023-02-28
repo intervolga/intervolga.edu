039b401df2234cd99b62325ad7e7f61fb1bf77f4
liliya <liliya@intervolga.ru>
2023-02-28 12:31:38 +0300

    [IVEDU-87] предупреждение при копировании ссылки (http соединение)

--- admin.js
+++ admin.js
@@ -1,17 +1,26 @@
 $(document).on('click', '.iv-copy-link', function() {
-	window.navigator.clipboard.writeText($(this).attr('data-url'))
+	if (typeof window.navigator.clipboard == 'undefined') {
+		result=confirm('Ошибка копирования пути: страница использует незащищенное http соединение.\n' +
+			'Использовать https соединение?');
+		if (result) {
+			window.location.href=window.location.href.replace(/http:/i, 'https:');
+		}
+	} else {
+		window.navigator.clipboard.writeText($(this).attr('data-url'))
+	}
 });
 
-BX.ready(function () {
+BX.ready(function() {
 	intervolgaEduAutoSelectTab();
 });
 
 function intervolgaEduAutoSelectTab() {
-	var tab = BX.getCookie('intervolga_edu_tab');
+	var tab=BX.getCookie('intervolga_edu_tab');
 	if (tab && tab.length) {
 		tabControl.SelectTab(tab);
 	}
 }
+
 function intervolgaEduOnTabChanged(tab) {
 	BX.setCookie('intervolga_edu_tab', tab);
 }
