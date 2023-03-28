057efc353dfe6f6de7de8d004d8293038c598eb9
liliya <liliya@intervolga.ru>
2023-03-28 13:19:06 +0300

    [IVEDU-93] проверка индексации и  URL у ИБ респонденты и результаты опроса

---
+++ lang/ru/lib/tests/course3/lesson3/testresultspollingiblock.php
@@ -0,0 +1,6 @@
+<?php
+$MESS['INTERVOLGA_EDU.COURSE_3_LESSON_3_SETTINGS_NOT_EMPTY_LIST_PAGE_URL'] = 'В настройках инфоблока <b>URL страницы информационного блока</b> должен быть пустым, сейчас #VALUE#';
+$MESS['INTERVOLGA_EDU.COURSE_3_LESSON_3_SETTINGS_NOT_EMPTY_DETAIL_PAGE_URL'] = 'В настройках инфоблока <b>URL страницы детального просмотра</b> должен быть пустым, сейчас #VALUE#';
+$MESS['INTERVOLGA_EDU.COURSE_3_LESSON_3_SETTINGS_NOT_EMPTY_SECTION_PAGE_URL'] = 'В настройках инфоблока <b>URL страницы раздела</b> должен быть пустым, сейчас #VALUE#';
+$MESS['INTERVOLGA_EDU.COURSE_3_LESSON_3_SETTINGS_NOT_EMPTY_INDEX_ELEMENT'] = 'В настройках инфоблока <b>Индексировать элементы для модуля поиска</b> должен быть пустым, сейчас #VALUE#';
+$MESS['INTERVOLGA_EDU.COURSE_3_LESSON_3_SETTINGS_NOT_EMPTY_INDEX_SECTION'] = 'В настройках инфоблока <b>Индексировать разделы для модуля поиска</b> должно быть отключено, сейчас #VALUE#';
