cad6786688539db192aec1b444a67be2d4e2d199
liliya <liliya@intervolga.ru>
2023-03-29 13:59:18 +0300

    [IVEDU-93] проверка шаблона компонента на содержание запрещенных слов

--- lib/tests/course3/lesson3/testrespondentcomponent.php
+++ lib/tests/course3/lesson3/testrespondentcomponent.php
@@ -1,20 +1,49 @@
 <?php
 namespace Intervolga\Edu\Tests\Course3\Lesson3;
 
+use Bitrix\Main\Localization\Loc;
+use Intervolga\Edu\Asserts\Assert;
+use Intervolga\Edu\Asserts\AssertComponent;
 use Intervolga\Edu\FilesTree\Component\Component;
 use Intervolga\Edu\FilesTree\Component\SimpleComponent;
+use Intervolga\Edu\FilesTree\ComponentTemplate;
+use Intervolga\Edu\FilesTree\FilesTree;
+use Intervolga\Edu\Locator\Component\RespondentsComponent;
+use Intervolga\Edu\Locator\IO\ComponentTemplate\RespondentsTemplate;
 use Intervolga\Edu\Locator\IO\CustomRespondents;
 use Intervolga\Edu\Locator\IO\DirectoryLocator;
 use Intervolga\Edu\Tests\BaseComponentTest;
+use Intervolga\Edu\Util\Regex;
 
 class TestRespondentComponent extends BaseComponentTest
 {
+	protected static function run()
+	{
+		AssertComponent::componentLocator(RespondentsComponent::class);
+		if (RespondentsComponent::find()) {
+			parent::run();
+		}
+	}
+
+	protected static function checkTemplateDirectory()
+	{
+		$locatorTemplate = static::getTemplateLocator();
+		Assert::directoryLocator($locatorTemplate);
+		if ($templateDir = $locatorTemplate::find(static::getComponentTemplateTree()::getTemplateTree())) {
+			/**
+			 * @var ComponentTemplate $templateDir
+			 */
+			static::testTemplateCode($templateDir);
+			static::checkFileContent($templateDir);
+		}
+	}
+
 	/**
 	 * @return string|DirectoryLocator
 	 */
-	protected static function getLocator()
+	protected static function getTemplateLocator()
 	{
-		return CustomRespondents::class;
+		return RespondentsTemplate::class;
 	}
 
 	/**
@@ -25,10 +54,34 @@ class TestRespondentComponent extends BaseComponentTest
 		return SimpleComponent::class;
 	}
 
+	protected static function testTemplateCode(FilesTree $templateDir)
+	{
+		$files = [];
+		foreach ($templateDir->getChildren() as $file) {
+			if ($file->isFile() && $file->getExtension() == 'php') {
+				$files[] = $file->getPath();
+			}
+		}
+		Assert::phpSniffer($files, [
+			'general',
+		]);
+	}
+
+	protected static function checkFileContent(ComponentTemplate $templateDir)
+	{
+		foreach ($templateDir->getChildren() as $child) {
+			if ($child->isFile() && $child->getExtension() == 'php') {
+				Assert::fileContentNotMatches($child, new Regex('/(Мужчина|Женщина|Заработная плата|Пол|Возраст)/iu',
+					Loc::getMessage('INTERVOLGA_EDU.COURSE_3_LESSON_3_FILE_CONTAINS_INVALID_WORLDS')));
+			}
+			//todo: lang-папку проверить
+		}
+	}
+
 	/**
 	 * @return string|DirectoryLocator
 	 */
-	protected static function getTemplateLocator()
+	protected static function getLocator()
 	{
 		return CustomRespondents::class;
 	}
