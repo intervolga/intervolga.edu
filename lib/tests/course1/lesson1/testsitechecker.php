5e9206e080fe577c50a2019225afbdbb25820369
liliya <whitelacoste@mail.ru>
2023-04-14 13:19:33 +0300

    [IVEDU-97] перенесла языковые в lang-файлы, проверка последнего изменения файла логов проверки системы

--- lib/tests/course1/lesson1/testsitechecker.php
+++ lib/tests/course1/lesson1/testsitechecker.php
@@ -4,6 +4,7 @@ namespace Intervolga\Edu\Tests\Course1\Lesson1;
 use Bitrix\Main\Application;
 use Bitrix\Main\IO\File;
 use Bitrix\Main\Localization\Loc;
+use Bitrix\Main\Type\Date;
 use Intervolga\Edu\Asserts\Assert;
 use Intervolga\Edu\Tests\BaseTest;
 
@@ -19,6 +20,7 @@ class TestSiteChecker extends BaseTest
 		$re = '/(?<DATE>\d{4}-.{3}-\d{2} \d{2}:\d{2}:\d{2}) (?<TITLE>.*) \((?<CODE>.*)\): Fail\n(?<ERROR>.*)/m';
 		preg_match_all($re, $logFile->getContents(), $matches, PREG_SET_ORDER);
 
+		Assert::greaterEq($logFile->getModificationTime(), (new Date())->add('-3 day'), Loc::getMessage('INTERVOLGA_EDU.COURSE_1_LESSON_1_SITE_CHECK_LOG_EXPIRED'));
 		Assert::count($matches, 0);
 	}
 }
