102a9fd68cf8295984c3471f161957491cdcd4ae
chernikov <chernikov@intervolga.ru>
2023-04-20 10:31:14 +0300

    [IVEDU-101] - Сделал Popup

--- lib/eventhandlers/main.php
+++ lib/eventhandlers/main.php
@@ -6,6 +6,22 @@ class Main
 	function OnPanelCreateHandler()
 	{
 		global $APPLICATION;
+		$action = $APPLICATION->GetPopupLink([
+			"URL" => "/bitrix/admin/settings.php?lang=ru&mid=intervolga.edu",
+			"PARAMS" => [
+				"width" => 780,
+				"height" => 570,
+				"resizable" => true,
+				"min_width" => 780,
+				"min_height" => 400
+			]]);
+		$menu = [];
+		$menu[] = [
+			'TEXT' => 'В popup\'е',
+			'ACTION' => $action,
+
+		];
+
 		$APPLICATION->AddPanelButton([
 			'ID' => '228',
 			'TEXT' => 'Проверка академии',
@@ -13,6 +29,7 @@ class Main
 			'SRC' => '/bitrix/images/intervolga.edu/logo-academy.png',
 			'TYPE' => 'BIG',
 			'MAIN_SORT' => 1400,
+			'MENU' => $menu
 		]);
 
 	}
