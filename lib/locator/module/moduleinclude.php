e660abf879524b9419813bcf9e4073538ca1c148
liliya <whitelacoste@mail.ru>
2023-04-25 16:04:44 +0300

    [IVEDU-110] дополнила проверкой на подключение классов кастом модуля в incude.php

---
+++ lib/locator/module/moduleinclude.php
@@ -0,0 +1,19 @@
+<?php
+namespace Intervolga\Edu\Locator\Module;
+
+use Bitrix\Main\Localization\Loc;
+
+class ModuleInclude extends ModuleFileLocator
+{
+    public static function getNameLoc(): string
+    {
+        return Loc::getMessage('INTERVOLGA_EDU.MODULE_INCLUDE');
+    }
+
+    protected static function getPaths(): array
+    {
+        return [
+            '/include.php'
+        ];
+    }
+}
--- lib/tests/course3/lesson2/testclasses.php
+++ lib/tests/course3/lesson2/testclasses.php
@@ -4,18 +4,19 @@ namespace Intervolga\Edu\Tests\Course3\Lesson2;
 use Intervolga\Edu\Asserts\Assert;
 use Intervolga\Edu\Locator\ClassLocator\CustomModuleParentTable;
 use Intervolga\Edu\Locator\ClassLocator\CustomModuleTable;
+use Intervolga\Edu\Locator\Module\ModuleInclude;
 use Intervolga\Edu\Tests\BaseTest;
+use Intervolga\Edu\Util\Regex;
 
 class TestClasses extends BaseTest
 {
-	public static function interceptErrors()
-	{
-		return true;
-	}
-
 	protected static function run()
 	{
-		Assert::classLocator(CustomModuleParentTable::class);
+        Assert::moduleFileExists(ModuleInclude::class);
+        Assert::fileContentMatches(ModuleInclude::find(),
+            new Regex('/Loader::registerAutoLoadClasses/', 'Loader::registerAutoLoadClasses()'));
+
+        Assert::classLocator(CustomModuleParentTable::class);
 		Assert::classLocator(CustomModuleTable::class);
 		if ($class = CustomModuleTable::find()) {
 			Assert::phpSniffer([$class->getFileName()], ['oldOrmClass']);
