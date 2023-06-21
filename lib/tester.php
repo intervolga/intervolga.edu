eec52ce77eeecebc305f99302bbddb5f029e32f8
liliya <liliya@intervolga.ru>
2023-06-21 16:09:18 +0300

    [IVEDU-116] Проверка компонента "слайдер" (размещение, общая проверка кода)

--- lib/tester.php
+++ lib/tester.php
@@ -88,6 +88,7 @@ class Tester
 			\Intervolga\Edu\Tests\Course1\Lesson11\TestSmartFilterIsExist::class,
 			\Intervolga\Edu\Tests\Course1\Lesson11\TestPropertyInFilter::class,
 
+			\Intervolga\Edu\Tests\Course2\Lesson1_2\TestSliderComponent::class,
 			\Intervolga\Edu\Tests\Course2\Lesson1_2\TestCatalogBindingProperty::class,
 			\Intervolga\Edu\Tests\Course2\Lesson1_2\TestPropertyPrice::class,
 
@@ -118,7 +119,7 @@ class Tester
 			\Intervolga\Edu\Tests\Course2\Lesson7\TestSecureAuthorization::class,
 
 			\Intervolga\Edu\Tests\Course2\Lesson8\TestGadgetTemplate::class,
-			\Intervolga\Edu\Tests\Course2\Lesson8\TestSettingResultLinks::class,
+			//\Intervolga\Edu\Tests\Course2\Lesson8\TestSettingResultLinks::class,
 			\Intervolga\Edu\Tests\Course2\Lesson8\TestDesktopPage::class,
 
 
