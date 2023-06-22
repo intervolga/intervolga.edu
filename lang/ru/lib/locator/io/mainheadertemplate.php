336111d9d8038df71146dee849b4ac0312d66214
liliya <liliya@intervolga.ru>
2023-06-22 12:14:27 +0300

    [IVEDU-17] Заменила проверку расположения слайдера

---
+++ lang/ru/lib/locator/io/mainheadertemplate.php
@@ -0,0 +1,2 @@
+<?php
+$MESS['INTERVOLGA_EDU.MAIN_HEADER_TEMPLATE'] = 'Шаблон header главной страницы';
--- lib/tests/course2/lesson1_2/testcomponentinclude.php
+++ lib/tests/course2/lesson1_2/testcomponentinclude.php
@@ -1,24 +1,28 @@
 <?php
 namespace Intervolga\Edu\Tests\Course2\Lesson1_2;
 
+use Bitrix\Main\Localization\Loc;
+use Intervolga\Edu\Asserts\Assert;
 use Intervolga\Edu\Asserts\AssertComponent;
 use Intervolga\Edu\Locator\Component\Template\Slider;
-use Intervolga\Edu\Locator\Component\Template\TemplateLocator;
+use Intervolga\Edu\Locator\IO\MainHeaderTemplate;
 use Intervolga\Edu\Tests\BaseTest;
+use Intervolga\Edu\Util\Regex;
 
 class TestComponentInclude extends BaseTest
 {
+	const REG_INCLUDE_SLIDER = '/\$APPLICATION-\>IncludeComponent\(\s*(\'|")bitrix\:news\.list(\'|")\,\s*(\'|")#SLID2ER#/i';
+
 	protected static function run()
 	{
-		AssertComponent::TemplateLocator(static::getLocator());
+		AssertComponent::componentLocator(Slider::class);
+		if ($slider = Slider::find()) {
+			Assert::fileLocator(MainHeaderTemplate::class);
+			if ($page = MainHeaderTemplate::find()) {
+				$regex = str_replace('#SLIDER#', $slider['TEMPLATE_NAME'], static::REG_INCLUDE_SLIDER);
+				Assert::fileContentMatches($page, new Regex($regex,
+					Loc::getMessage('INTERVOLGA_EDU.COURSE_2.LESSON_1_2.WRONG_PATH_SLIDER')));
+			}
+		}
 	}
-
-	/**
-	 * @return string|TemplateLocator
-	 */
-	protected static function getLocator()
-	{
-		return Slider::class;
-	}
-
 }
