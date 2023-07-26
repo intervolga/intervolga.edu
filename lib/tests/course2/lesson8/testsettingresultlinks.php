c4632861249e6f4a3caf183cd66a73736079d3d1
liliya <liliya@intervolga.ru>
2023-07-26 14:53:20 +0300

    [IVEDU-119] Правки регулярного выражения для поиска ссылок

--- lib/tests/course2/lesson8/testsettingresultlinks.php
+++ lib/tests/course2/lesson8/testsettingresultlinks.php
@@ -18,7 +18,7 @@ class TestSettingResultLinks extends BaseTest
 
 	public static function interceptErrors()
 	{
-		return true;
+		return false;
 	}
 
 	protected static function run()
@@ -128,9 +128,9 @@ class TestSettingResultLinks extends BaseTest
 		$page = ob_get_contents();
 		ob_clean();
 
-		preg_match_all('/href="[\w\s\d\=\'"\/_\.\?&;\-\#\$\%\^:]*\>/iu', $page, $matches, PREG_SET_ORDER);
+		preg_match_all('/href\s*=\s*("|\')[\w\s\d=\/_.?&;\-#]*("|\')/iu', $page, $matches, PREG_SET_ORDER);
 		foreach ($matches as $match) {
-			$urls[] = mb_strcut($match[0], strpos($match[0], '"') + 1, -2);
+			$urls[] = mb_strcut(trim($match[0]), 6, -1);
 		}
 
 		if (strpos($urls[1], 'find_date_create_1')) {
