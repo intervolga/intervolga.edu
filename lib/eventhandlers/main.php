4f1501ae922c561e77dea4745bcd3aa4f96872a5
chernikov <chernikov@intervolga.ru>
2023-09-19 18:08:25 +0300

    [IVEDU-101] - Вынес кириллицу в lang-файлы

--- lib/eventhandlers/main.php
+++ lib/eventhandlers/main.php
@@ -1,36 +1,27 @@
 <?php
+
 namespace Intervolga\Edu\Eventhandlers;
+
+use Bitrix\Main\Localization\Loc;
+
+Loc::loadMessages(__FILE__);
 
 class Main
 {
-	function OnPanelCreateHandler()
-	{
-		global $APPLICATION;
-		$action = $APPLICATION->GetPopupLink([
-			"URL" => "/bitrix/admin/settings.php?lang=ru&mid=intervolga.edu",
-			"PARAMS" => [
-				"width" => 780,
-				"height" => 570,
-				"resizable" => true,
-				"min_width" => 780,
-				"min_height" => 400
-			]]);
-		$menu = [];
-		$menu[] = [
-			'TEXT' => 'В popup\'е',
-			'ACTION' => $action,
+    function OnPanelCreateHandler()
+    {
+        global $APPLICATION;
+        $url = '/bitrix/admin/settings.php?lang=ru&mid=intervolga.edu';
 
-		];
 
-		$APPLICATION->AddPanelButton([
-			'ID' => '228',
-			'TEXT' => 'Проверка академии',
-			'HREF' => '/bitrix/admin/settings.php?lang=ru&mid=intervolga.edu',
-			'SRC' => '/bitrix/images/intervolga.edu/logo-academy.png',
-			'TYPE' => 'BIG',
-			'MAIN_SORT' => 1400,
-			'MENU' => $menu
-		]);
+        $APPLICATION->AddPanelButton([
+            'ID' => '230',
+            'TEXT' => Loc::getMessage('INTERVOLGA_EDU.MODULE_NAME'),
+            'HREF' => $url,
+            'SRC' => '/bitrix/images/intervolga.edu/logo-academy.png',
+            'TYPE' => 'BIG',
+            'MAIN_SORT' => 1400,
+        ]);
 
-	}
+    }
 }
