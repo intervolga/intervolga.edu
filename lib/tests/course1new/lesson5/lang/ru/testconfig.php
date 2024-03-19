5434d221ff6e7c5a0ef5f2744f718496c4b463a8
liliya <liliya@intervolga.ru>
2024-03-19 13:30:35 +0300

    [IVEDU-134] - NEW-ACAD 1-5 Убрала проверку на ограничение по времени.

--- lib/tests/course1new/lesson5/lang/ru/testconfig.php
+++ lib/tests/course1new/lesson5/lang/ru/testconfig.php
@@ -1,2 +1,3 @@
 <?php
+$MESS['IV_EDU.NEW_ACADEMY.C_1.L_5.EMPTY_SITE_CHECK_LOG'] = 'Проверка системы не запускалась (Не удалось найти логи проверки)';
 $MESS['IV_EDU.NEW_ACADEMY.C_1.L_5.CONFIG_ERRORS'] = 'Количество ошибок при полном тестировании системы <b>#VALUE#</b>';
--- lib/tests/course1new/lesson5/testconfig.php
+++ lib/tests/course1new/lesson5/testconfig.php
@@ -4,7 +4,6 @@ namespace Intervolga\Edu\Tests\Course1New\Lesson5;
 use Bitrix\Main\Application;
 use Bitrix\Main\IO\File;
 use Bitrix\Main\Localization\Loc;
-use Bitrix\Main\Type\Date;
 use Intervolga\Edu\Asserts\Assert;
 use Intervolga\Edu\Tests\BaseTest;
 
@@ -14,11 +13,10 @@ class TestConfig extends BaseTest
 	{
 		$checkerTest = new \CSiteCheckerTest();
 		$logFile = new File(Application::getDocumentRoot() . $checkerTest->LogFile);
-		Assert::fseExists($logFile, Loc::getMessage('INTERVOLGA_EDU.COURSE_1_LESSON_1_EMPTY_SITE_CHECK_LOG'));
+		Assert::fseExists($logFile, Loc::getMessage('IV_EDU.NEW_ACADEMY.C_1.L_5.EMPTY_SITE_CHECK_LOG'));
 		$re = '/(?<DATE>\d{4}-.{3}-\d{2} \d{2}:\d{2}:\d{2}) (?<TITLE>.*) \((?<CODE>.*)\): Fail\n(?<ERROR>.*)/m';
 		preg_match_all($re, $logFile->getContents(), $matches, PREG_SET_ORDER);
 
-		Assert::greaterEq($logFile->getModificationTime(), (new Date())->add('-3 day'), Loc::getMessage('INTERVOLGA_EDU.COURSE_1_LESSON_1_SITE_CHECK_LOG_EXPIRED'));
 		Assert::count($matches, 0, Loc::getMessage('IV_EDU.NEW_ACADEMY.C_1.L_5.CONFIG_ERRORS'));
 	}
 }
