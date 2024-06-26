3a8de0951ab86d9cbd90c56f30e5a61f2485e363
liliya <liliya@intervolga.ru>
2024-06-26 12:31:15 +0300

    [IVEDU-142] TestPropertyCode

--- lib/tests/course3/lesson3/testpropertycode.php
+++ lib/tests/course3/lesson3/testpropertycode.php
@@ -10,41 +10,43 @@ use Intervolga\Edu\Util\Regex;
 
 class TestPropertyCode extends BaseTest
 {
-    public static function interceptErrors()
-    {
-        return false;
-    }
+	public static function interceptErrors()
+	{
+		return true;
+	}
 
-    protected static function run()
-    {
-        static::checkComponentFile(CustomRespondents::class);
-        static::checkTemplate(RespondentTemplate::class);
-    }
+	protected static function run()
+	{
+		static::checkComponentFile(CustomRespondents::class);
+		static::checkTemplate(RespondentTemplate::class);
+	}
 
-    /**
-     * @param DirectoryLocator|string $dirComponent
-     */
-    protected static function checkComponentFile($dirComponent)
-    {
-        Assert::directoryLocator($dirComponent);
-        $file = $dirComponent::getComponentFile();
-        Assert::fileContentNotMatches($file,
-            new Regex('/(\'|")(SALARY|GENDER|AGE)(\'|")/i', '\'SALARY\', \'GENDER\', \'AGE\''));
-    }
+	/**
+	 * @param DirectoryLocator|string $dirComponent
+	 */
+	protected static function checkComponentFile($dirComponent)
+	{
+		Assert::directoryLocator($dirComponent);
+		if ($dirComponent::find()) {
+			$file = $dirComponent::getComponentFile();
+			Assert::fileContentNotMatches($file,
+				new Regex('/(\'|")(SALARY|GENDER|AGE)(\'|")/i', '\'SALARY\', \'GENDER\', \'AGE\''));
+		}
+	}
 
-    /**
-     * @param DirectoryLocator|string $dirTemplate
-     */
-    protected static function checkTemplate($dirTemplate)
-    {
-        Assert::directoryLocator($dirTemplate);
-        $directory = $dirTemplate::find();
-
-        foreach ($directory->getChildren() as $child) {
-            if ($child->isFile()) {
-                Assert::fileContentNotMatches($child,
-                    new Regex('/(\'|")(SALARY|GENDER|AGE)(\'|")/i', '\'SALARY\', \'GENDER\', \'AGE\''));
-            }
-        }
-    }
+	/**
+	 * @param DirectoryLocator|string $dirTemplate
+	 */
+	protected static function checkTemplate($dirTemplate)
+	{
+		Assert::directoryLocator($dirTemplate);
+		if ($directory = $dirTemplate::find()) {
+			foreach ($directory->getChildren() as $child) {
+				if ($child->isFile()) {
+					Assert::fileContentNotMatches($child,
+						new Regex('/(\'|")(SALARY|GENDER|AGE)(\'|")/i', '\'SALARY\', \'GENDER\', \'AGE\''));
+				}
+			}
+		}
+	}
 }
