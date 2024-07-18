e6293a603f1e37d2dd16490744d1d7ac5e4229d5
liliya <liliya@intervolga.ru>
2024-07-18 11:39:06 +0300

    [IVEDU-101] - Правка совместимости

--- lib/eventhandlers/main.php
+++ lib/eventhandlers/main.php
@@ -8,20 +8,17 @@ Loc::loadMessages(__FILE__);
 
 class Main
 {
-    function OnPanelCreateHandler()
-    {
-        global $APPLICATION;
-        $url = '/bitrix/admin/settings.php?lang=ru&mid=intervolga.edu';
+	public static function OnPanelCreateHandler()
+	{
+		global $APPLICATION;
+		$url = '/bitrix/admin/settings.php?lang=ru&mid=intervolga.edu';
 
-
-        $APPLICATION->AddPanelButton([
-            'ID' => '230',
-            'TEXT' => Loc::getMessage('INTERVOLGA_EDU.MODULE_NAME'),
-            'HREF' => $url,
-            'SRC' => '/bitrix/images/intervolga.edu/logo-academy.png',
-            'TYPE' => 'BIG',
-            'MAIN_SORT' => 1400,
-        ]);
-
-    }
+		$APPLICATION->AddPanelButton([
+			'TEXT' => Loc::getMessage('INTERVOLGA_EDU.MODULE_NAME'),
+			'HREF' => $url,
+			'SRC' => '/bitrix/images/intervolga.edu/logo-academy.png',
+			'TYPE' => 'BIG',
+			'MAIN_SORT' => 1400,
+		]);
+	}
 }
